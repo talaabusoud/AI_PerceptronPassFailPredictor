94a4f8a728708f2b06a7413465ef3601655f9a94
Tala Abu Soud <talaashrafsoud@gmail.com>
2024-12-21 02:48:36 +0200

    passPrediction

---
+++ src/main/java/com/passprediction/Row.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.passprediction;
+
+/**
+ *
+ * @author RayaThawabe
+ */
+public class Row {
+
+    public float[] marks;
+    public boolean pass;
+    
+    public boolean isPass(){
+        return pass;
+    }
+    
+    public float[] normalizeMarks(int mid){
+        float[] normalizedMarks = new float[marks.length];
+        for(int i = 0; i < marks.length; i++){
+            normalizedMarks[i] = (marks[i]-mid)/(float)(100-mid);
+        }
+        return normalizedMarks;
+    }
+}
